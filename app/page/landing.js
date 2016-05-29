8c0b33e768eb253f4abd96138ae45ba6377620d1
Sebastian Zorn <sebi.zorn@googlemail.com>
2016-05-29 09:27:36 +0200

    Better styled starting screen

--- app/page/landing.js
+++ app/page/landing.js
@@ -8,10 +8,10 @@ class LandingPage extends React.Component {
 				<section className="jumbotron text-xs-center">
 		      <div className="container">
 		        <h1 className="jumbotron-heading">9Tac</h1>
-		        <p className="lead text-muted">A simple game </p>
+		        <p className="lead text-muted">This is an variation of the classic tic tac toe game. The board is composed of a 3x3 grid of Big Squares. Inside of each Big Square is another 3x3 grid, so there are 9 Small Squares inside each Big Square</p>
 		        <p>
-		          <Link to="/game" className="btn btn-primary">Start</Link>
-							<Link to="/game-join" className="btn btn-secondary">Join</Link>
+		          <Link to="/game" className="btn btn-block btn-primary">Start</Link>
+							<Link to="/game-join" className="btn btn-block btn-secondary">Join</Link>
 		        </p>
 		      </div>
 	    	</section>
